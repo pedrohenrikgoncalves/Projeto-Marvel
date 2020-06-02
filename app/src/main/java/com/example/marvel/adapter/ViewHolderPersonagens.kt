b30c0927cd8833f1ea1a6434313311ed8e31706e
Joao Felipe <joaocamargo2205@gmail.com>
2020-06-01 21:27:11 -0300

    Ajudando pedro coolearning

---
+++ app/src/main/java/com/example/marvel/adapter/ViewHolderPersonagens.kt
@@ -0,0 +1,15 @@
+package com.example.marvel.adapter
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.marvel.R
+
+class ViewHolderPersonagens(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var imageView: ImageView = itemView.findViewById(R.id.imagePersonagens)
+    var heroi: TextView = itemView.findViewById(R.id.text_titulo)
+    var cardViewCharacters: CardView = itemView.findViewById(R.id.card_view_characters)
+
+}
