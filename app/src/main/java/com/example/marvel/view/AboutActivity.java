690a3fc911ec3d37d76325fe26a184b5983baf32
Pedro Henrik <37743604+pedrohenrikgoncalves@users.noreply.github.com>
2020-06-18 22:32:52 -0300

    Click no Goot com um toast

--- app/src/main/java/com/example/marvel/view/AboutActivity.java
+++ app/src/main/java/com/example/marvel/view/AboutActivity.java
@@ -3,12 +3,21 @@ package com.example.marvel.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.marvel.R;
 
+import static android.widget.Toast.makeText;
+
 public class AboutActivity extends AppCompatActivity {
+
+    ImageView goot;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +27,14 @@ public class AboutActivity extends AppCompatActivity {
         Window window = getWindow();
         window.setStatusBarColor(getColor(R.color.colorSplash));
         getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorSplash));
+
+        goot = findViewById(R.id.imageGoot);
+
+        goot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeText(context, R.string.goot, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
